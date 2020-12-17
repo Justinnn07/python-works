ead88a7c056ae3363a25c1946ab384d383271ec3
Justinnn07 <justinv2006@gmail.com>
2020-12-18 00:45:06 +0530

    in progress

---
+++ app.py
@@ -0,0 +1,166 @@
+# data types
+justin = (str(13))
+"""
+str, float, type, variables, int
+"""
+# print(type(justin))
+
+
+# operators
+
+a = 3
+b = 2
+
+"""
+addition, subtraction, division(print) normally
+"""
+
+# exponents, remainders , flow division
+"""
+print(a%b)
+remainder
+"""
+
+"""
+print(a**b)
+exponents
+"""
+"""
+print(a//b)
+"""
+
+# inline function
+"""
+a+= 1
+print(a)
+
++ - * (expo + // )
+"""
+
+"""
+mylist = ["hello", 25, "bye", 100.01]
+
+# print(type(mylist))
+print(mylist[3])
+
+# str, type etc etc type must pe destructured in ordered to work with it
+"""
+
+# tuples
+
+"""
+mytups = ("ster", 1, "hello")
+print(mytups)
+"""
+
+# sets 
+"""
+mySet = {"abcd", 12, 12.9}
+"""
+
+# dictionaries
+
+"""
+myDict = {
+    "justin" : "varghese"
+}
+
+print(type(myDict))
+"""
+
+# if else
+
+"""
+x = 100
+y = 200
+z = 500
+
+if x>y or z>y  :
+    print("X is greater than y")
+elif x==y:
+    print("X is equal to y")
+else:
+    print("y is greater than x")
+"""
+
+# for loops
+
+# colors = ["red", "blue", "green" ]
+
+# for i in  range(10):print(i)
+
+"""
+b = 6
+
+for b in range(10):
+    print(b)
+    if b==6:
+       print("i am equal to 6")
+    else:
+        print("hiii")
+"""
+
+# While loops
+"""
+k = 120
+
+while k > 100:
+    print("i am greater than 100")
+    k = k-5 
+print(" i am out")
+"""
+
+"""
+kiran = "          I am the header           "
+akshay = "I AM TEH HEADER"
+
+# print(kiran.replace("a", "1233"))
+
+yui = kiran.strip()
+
+# print(yui)
+
+print(akshay.lower())
+"""
+
+# print(6+6)
+
+"""
+kiran = 100
+
+str = "I want {} mangoes"
+
+print(str.format(kiran))
+"""
+
+"""
+kiran = 900
+vishakh = 600
+akshay = 800
+
+str = "Akshay {} Vishakh {} Kiran"
+
+print(str.format(akshay, vishakh, kiran))
+"""
+
+"""
+
+myList = [89, 99, 11]
+myList1 = [99, 11, 12]
+
+x = myList+myList1
+
+x.sort()
+
+print(x)
+"""
+
+### Append
+
+
+myList = [89, 99, 11]
+myList1 = [99, 11, 12]
+
+for x in myList1:
+    myList.append(x)
+print(myList)
